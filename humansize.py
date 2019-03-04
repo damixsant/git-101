0db2d0b6803eebc1bb81482ca1ce09cb4c7071e4
Damiano Santarcangelo <damixsant10@gmail.com>
2019-03-04 19:08:29 +0100

    add documenatation

--- humansize.py
+++ humansize.py
@@ -2,9 +2,10 @@ suffixes= ['KB','MB','GB','TB','PB','EB','ZB','YB']
 
 
 def approximate_size(size):
+    """Convert a file size to human-readable form."""
     if size <0:
         raise ValueError ('number must be non-negative')
-        
+
     multiple= 1024
     for suffix in suffixes:
         size/=suffixes[multiple]:
